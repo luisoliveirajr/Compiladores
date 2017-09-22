9d83c45b73615ac146b3a41408e57e221fd9161d
luis.oliveira <luis.oliveira@pat83035>
2017-09-21 22:10:34 -0300

    Compiladores

--- Compilador/src/Model/Operations.java
+++ Compilador/src/Model/Operations.java
@@ -6,6 +6,8 @@
 package Model;
 
 import java.util.Stack;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 /**
  *
@@ -16,32 +18,100 @@ public class Operations {
     
     //Metódo converte expressão de infixa para posfixa.
     public String Posfixa(String s){
-        s = ExConc(s);
-        
-        
-        return s;
+        int i;
+        String pos = new String();
+        //Alertta de Erros.
+        Alert alerta = new Alert(AlertType.ERROR);
+        alerta.setTitle("Compilador");
+        alerta.setHeaderText(null);
+            s = ExConc(s);
+            //Primeiro Algoritmo.
+            for(i=0; i< s.length(); i++){
+                //Verifica se o caracter é operando    
+                if(s.charAt(i)!= '(' && s.charAt(i)!= ')' && s.charAt(i)!= '.' && s.charAt(i)!= '+' && s.charAt(i)!= '*' ){
+
+                    pos += s.charAt(i);
+                }
+                //Caso o caracter seja operador.
+                else{ 
+                    //Se for abre parenteses.
+                    if(s.charAt(i)== '('){
+                         pilha.push('(');
+                    }
+                    //Se for fecha parentese
+                    else if(s.charAt(i)== ')'){
+                            //Desempilha até enquanto a pilha não estiver vazia ou encontrar o abre parentese.  
+                            while(!pilha.isEmpty() && pilha.lastElement() != '(' ){
+                              pos+= pilha.lastElement();
+                              pilha.pop();
+                            }
+                            //Remove o último elemento da pilha. 
+                            if(!pilha.empty()){
+                                pilha.pop();
+                            }   
+                            else{
+                                alerta.setContentText("Falta abrir parêntese '('.");
+                                alerta.showAndWait();  
+                            }
+                    }
+                     // 
+                    else{
+                          if(!pilha.empty()){                         
+                             if(PreOp(s.charAt(i))> PreOp(pilha.lastElement())){
+                                pilha.push(s.charAt(i));                           
+                             }
+                             else{
+                                  while(!pilha.isEmpty() && PreOp(s.charAt(i)) <= PreOp(pilha.lastElement())){
+                                        pos+= pilha.lastElement(); 
+                                        pilha.pop();
+                                  }
+                                  pilha.push(s.charAt(i));                             
+                             }
+                          }
+                          else{
+                              pilha.push(s.charAt(i));  
+                          }
+                    }
+                }
+            }//fim for
+            //Desempilha todos operacdores que estão na pilha.
+            while(!pilha.isEmpty() && pilha.lastElement() != '(' ){
+                   pos+= pilha.lastElement();
+                   pilha.pop();
+            }
+            if(!pilha.empty()){
+                alerta.setContentText("Falta fechar parêntese ')'.");
+                alerta.showAndWait(); 
+            }
+        return pos;
     }
     
     //Metódo que torna explicito as concatenações da expressão
     private String ExConc(String a){
         String b = new String();
         int i;
-            for(i=0; i< a.length(); i++){
-            //Verifica se o caracter não é reservado 
-                if(i > 0 && a.charAt(i)!= ')' && a.charAt(i)!= '.' && a.charAt(i)!= '+' && a.charAt(i)!= '*'){
-                    //Verificar se o caracter onde será explicitado a contactenação não é reservado 
-                    if(a.charAt(i-1)!= '(' && a.charAt(i-1)!= ')' && a.charAt(i-1)!= '+' && a.charAt(i-1)!= '.'){
-                        
-                        b += '.'; 
-                    }  
+             for(i=0; i< a.length(); i++){
+                //Verifica se o caracter não é reservado 
+                if(a.charAt(i) == ' '){
+                   b += a.charAt(i);
+                   b += '.';
                 }
-                b += a.charAt(i);
+                else if(i > 0 && a.charAt(i) == '(' && a.charAt(i-1) == ')'){
+                    b+='.';
+                }
+                if(i > 0 && a.charAt(i-1) == '*'){
+                    b+='.';
+                    b += a.charAt(i);
+                }
+                else{
+                    b += a.charAt(i);
+                }
             }
         return b;
     }
     
-    //Metódo que definer a precendecia de operadores;
-    private int preOp(char op){
+    //Metódo que define a precendecia de operadores;
+    private int PreOp(char op){
         if(op == '*') return 3; //Maior precendecia
             else if(op == '.') return 2;   
                 else if(op == '+') return 1;
